3c5300fe9588e0576b80e71e129dbfc8f52d2b28
cassidypignatello <cassidy.pignatello@gmail.com>
2014-10-23 18:19:38 -0400

    writes out assignment in psuedocode

--- artist_list.rb
+++ artist_list.rb
@@ -9,4 +9,9 @@ class ArtistList
   end
 end
 
+# accept artist list instructions as input
+# slice at every new line character to create seperate lists
+# find only pairs of artists which appear together in at least 50 seperate lists
+# write those pairs of artists to a new output file
+
 ArtistList.new
